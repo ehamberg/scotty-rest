1a86365320a2a06b51939813c1fcbd03212a7c9c
Erlend Hamberg <erlend@hamberg.no>
2015-02-17 09:09:29 +0100

    Stylish haskell fixes for tests file

--- test/Web/ScottyRestSpec.hs
+++ test/Web/ScottyRestSpec.hs
@@ -1,21 +1,21 @@
-{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE MultiWayIf        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Web.ScottyRestSpec (main, spec) where
 
-import Test.Hspec
-import Test.Hspec.Wai
-import Test.Hspec.Wai.Internal
-import Test.QuickCheck (Arbitrary, arbitrary, elements, property)
+import           Test.Hspec
+import           Test.Hspec.Wai
+import           Test.Hspec.Wai.Internal
+import           Test.QuickCheck         (Arbitrary, arbitrary, elements, property)
 
-import Web.Scotty.Trans hiding (get, post, put, patch, delete, request)
-import qualified Web.Scotty.Rest as Rest
-import Web.Scotty.Rest (RestConfig(..), StdMethod(..))
+import           Web.Scotty.Rest         (RestConfig (..), StdMethod (..))
+import qualified Web.Scotty.Rest         as Rest
+import           Web.Scotty.Trans        hiding (delete, get, patch, post, put, request)
 
-import Control.Monad (liftM)
-import Data.ByteString.Char8 (pack)
-import Data.String.Conversions (cs)
+import           Control.Monad           (liftM)
+import           Data.ByteString.Char8   (pack)
+import           Data.String.Conversions (cs)
 
 instance Arbitrary Rest.StdMethod where
   arbitrary = elements (enumFromTo minBound maxBound)
